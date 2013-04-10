4330a4649bf108dd67da53b6bd8c72535b644434
ie2147 <ie2147@micro14.ilab.columbia.edu>
2013-04-10 12:55:01 -0400

    VGA controller and raster talk to each other with tiles, snake1 and snake2. Included definitions file

--- de2_vga_controller.vhd
+++ de2_vga_controller.vhd
@@ -1,6 +1,19 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use work.definitions.all;
+
+-- PROTOCOL:
+		-- FOR ADDRESS = 00
+			--9-0: Y (LSB)
+			--19-10: X
+			--24-20: SPRITE SELECT
+			--25: 1=ADD, 0=REMOVE
+			--26-27: Which segment referring to
+			--			00=head, 01=second to head
+			--			10=second to tail 11=tail
+			--26-31: UNUSED (MSB)
+--
 
 entity snake_plus_vga is
   
@@ -10,9 +23,9 @@ entity snake_plus_vga is
     read       : in  std_logic;
     write      : in  std_logic;
     chipselect : in  std_logic;
-    address    : in  std_logic_vector(2 downto 0);
-    readdata   : out std_logic_vector(15 downto 0);
-    writedata  : in  std_logic_vector(15 downto 0);
+    address    : in  std_logic_vector(3 downto 0);
+    readdata   : out std_logic_vector(31 downto 0);
+    writedata  : in  std_logic_vector(31 downto 0);
 	 
 	 VGA_CLK,                         	-- Clock
     VGA_HS,                          	-- H_SYNC
@@ -31,10 +44,15 @@ end snake_plus_vga;
 architecture rtl of snake_plus_vga is
 
   type ram_type is array(2 downto 0) of
-            std_logic_vector(15 downto 0);
+            std_logic_vector(31 downto 0);
   signal RAM : ram_type;
   signal ram_address: signed(2 downto 0);
   signal led_chooser : integer;
+  
+
+  signal tiles : tiles_ram;
+  signal snake1 : snake_ram;
+  signal snake2  : snake_ram;
   
   signal clk25 : std_logic := '0';
 
@@ -60,12 +78,16 @@ begin
   process (clk)
   begin
     if rising_edge(clk) then
+	 
       if reset_n = '0' then
-        readdata <= (others => '0');      
-		  led_chooser <= 0;
+			readdata <= (others => '0');
+			tiles <= (others=>(others=>(others=>'0')));
+			snake1 <= (others=>(others=>'0'));
+			snake2 <= (others=>(others=>'0'));
+			led_chooser <= 0;
       else
-        if chipselect = '1' then
-          if address(2) = '0' then
+        if chipselect = '1' then -- This chip is right one
+          if address = "0000" then
             if read = '1' then
               readdata <= RAM(to_integer(ram_address));
             elsif write = '1' then
@@ -78,7 +100,7 @@ begin
           end if;
         else
 		  -- EXTRA STUFF HERE FOR DEBUGGING ONLY
-          leds <= RAM(led_chooser);
+          leds <= RAM(led_chooser)(15 downto 0);
 				if led_chooser > 1 then
 					leds(12) <= '1';
 				else
@@ -101,7 +123,11 @@ V1: entity work.de2_vga_raster port map (
     VGA_SYNC => VGA_SYNC,
     VGA_R => VGA_R,
     VGA_G => VGA_G,
-    VGA_B => VGA_B
+    VGA_B => VGA_B,
+	 
+	 TILES_IN => tiles,
+	 SNAKE1_IN => snake1,
+	 SNAKE2_IN => snake2
   );
 
 end rtl;

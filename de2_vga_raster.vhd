5b991e0aca341b2f1dc488498bae2f9b0dafad40
mdk2133 <mdk2133@micro10.ilab.columbia.edu>
2013-04-11 16:10:55 -0400

    adding sprites

--- de2_vga_raster.vhd
+++ de2_vga_raster.vhd
@@ -90,7 +90,239 @@ architecture rtl of de2_vga_raster is
 	type ram_type is array(5 downto 0) of std_logic_vector(255 downto 0);
 	signal SPRITES : ram_type;
 
+	-- sprites
+	type array_type_16x16 is array (15 downto 0) of unsigned (15 downto 0);
+	
+	-- snake head colorings
+	signal sprite_snake_head_g 	: array_type_16x16;
+	signal sprite_snake_head_r 	: array_type_16x16;
+	signal sprite_snake_head_w 	: array_type_16x16;
+	signal sprite_snake_head_b 	: array_type_16x16;
+
+	-- snake body colorings
+	signal sprite_snake_body	: array_type_16x16;
+	
+	-- snake tail colorings
+	signal sprite_snake_tail	: array_type_16x16;
+	
+	-- rabbit colorings
+	signal sprite_food_rabbit_y	: array_type_16x16;
+	signal sprite_food_rabbit_p	: array_type_16x16;
+	signal sprite_food_rabbit_b	: array_type_16x16;
+	signal sprite_food_rabbit_w	: array_type_16x16;
+	
+	-- mouse colorings, rabbit eyes can be used for mouse eyes
+	signal sprite_food_mouse_y	: array_type_16x16;
+	signal sprite_food_mouse_p	: array_type_16x16;
+	signal sprite_food_mouse_l	: array_type_16x16;
+	
+	-- edwards colorings
+	signal sprite_food_edwards	: array_type_16x16;
+	
+	signal sprite_powup_growth	: array_type_16x16;
+	
+	signal sprite_powup_speed	: array_type_16x16;
+	
+	signal sprite_powup_freeze	: array_type_16x16;
+	
+	signal sprite_wall			: array_type_16x16;
+	
+	signal sprite_P				: array_type_16x16;
+	signal sprite_1				: array_type_16x16;
+	signal sprite_2				: array_type_16x16;
+	signal sprite_W				: array_type_16x16;
+	signal sprite_I				: array_type_16x16;
+	signal sprite_N				: array_type_16x16;
+	signal sprite_S				: array_type_16x16;
+	signal sprite_exclam			: array_type_16x16;
+	signal sprite_pause			: array_type_16x16;
+	signal sprite_play			: array_type_16x16;
+	
 begin
+  
+  -- sprite definitions
+  
+  -- sprite snake head coloring
+  sprite_snake_head_g(0) 			<=	"0000111100000000";
+  sprite_snake_head_g(1) 			<=	"0001111111000000";
+  sprite_snake_head_g(2) 			<=	"0011111111110000";
+  sprite_snake_head_g(3) 			<=	"0111111111111000";
+  sprite_snake_head_g(4) 			<=	"0111111111111100";
+  sprite_snake_head_g(5) 			<=	"1111100011111100";
+  sprite_snake_head_g(6) 			<=	"1111100011111110";
+  sprite_snake_head_g(7) 			<=	"1111100011111100";
+  sprite_snake_head_g(8) 			<=	"1111111111100000";
+  sprite_snake_head_g(9) 			<=	"1111111111100000";
+  sprite_snake_head_g(10) 			<=	"1111111111111100";
+  sprite_snake_head_g(11) 			<=	"0111111111111110";
+  sprite_snake_head_g(12) 	   	<=	"0111111111111000";
+  sprite_snake_head_g(13) 	   	<=	"0011111111110000";
+  sprite_snake_head_g(14) 	   	<=	"0001111111000000";
+  sprite_snake_head_g(15) 	   	<=	"0000111100000000";
+  
+  -- sprite snake head tongue coloring
+  sprite_snake_head_r(0) 			<=	"0000000000000000";
+  sprite_snake_head_r(1) 			<=	"0000000000000000";
+  sprite_snake_head_r(2) 			<=	"0000000000000000";
+  sprite_snake_head_r(3) 			<=	"0000000000000000";
+  sprite_snake_head_r(4) 	  		<=	"0000000000000000";
+  sprite_snake_head_r(5) 			<=	"0000000000000000";
+  sprite_snake_head_r(6) 			<=	"0000000000000000";
+  sprite_snake_head_r(7) 			<=	"0000000000000011";
+  sprite_snake_head_r(8) 			<=	"0000000000011110";
+  sprite_snake_head_r(9) 			<=	"0000000000011110";
+  sprite_snake_head_r(10) 			<=	"0000000000000011";
+  sprite_snake_head_r(11) 			<=	"0000000000000000";
+  sprite_snake_head_r(12) 	   	<=	"0000000000000000";
+  sprite_snake_head_r(13) 	   	<=	"0000000000000000";
+  sprite_snake_head_r(14) 	   	<=	"0000000000000000";
+  sprite_snake_head_r(15) 	  	 	<= "0000000000000000";
+  
+  -- sprite snake head eye white coloring
+  sprite_snake_head_w(0) 			<=	"0000000000000000";
+  sprite_snake_head_w(1) 			<=	"0000000000000000";
+  sprite_snake_head_w(2) 			<=	"0000000000000000";
+  sprite_snake_head_w(3) 			<=	"0000000000000000";
+  sprite_snake_head_w(4) 			<= "0000000000000000";
+  sprite_snake_head_w(5) 			<=	"0000011100000000";
+  sprite_snake_head_w(6) 			<=	"0000010000000000";
+  sprite_snake_head_w(7) 			<=	"0000010000000000";
+  sprite_snake_head_w(8) 			<=	"0000000000000000";
+  sprite_snake_head_w(9) 			<=	"0000000000000000";
+  sprite_snake_head_w(10) 			<=	"0000000000000000";
+  sprite_snake_head_w(11) 			<=	"0000000000000000";
+  sprite_snake_head_w(12) 	  	 	<=	"0000000000000000";
+  sprite_snake_head_w(13) 	  	 	<=	"0000000000000000";
+  sprite_snake_head_w(14) 	  	 	<=	"0000000000000000";
+  sprite_snake_head_w(15) 	  	 	<=	"0000000000000000";
+  
+    -- sprite snake head eye black coloring
+  sprite_snake_head_b(0) 			<=	"0000000000000000";
+  sprite_snake_head_b(1) 			<=	"0000000000000000";
+  sprite_snake_head_b(2) 			<=	"0000000000000000";
+  sprite_snake_head_b(3) 			<=	"0000000000000000";
+  sprite_snake_head_b(4) 			<=	"0000000000000000";
+  sprite_snake_head_b(5) 			<=	"0000000000000000";
+  sprite_snake_head_b(6) 			<=	"0000001100000000";
+  sprite_snake_head_b(7) 			<=	"0000001100000000";
+  sprite_snake_head_b(8) 			<=	"0000000000000000";
+  sprite_snake_head_b(9) 			<=	"0000000000000000";
+  sprite_snake_head_b(10) 			<=	"0000000000000000";
+  sprite_snake_head_b(11) 			<=	"0000000000000000";
+  sprite_snake_head_b(12) 	  	 	<=	"0000000000000000";
+  sprite_snake_head_b(13) 	  	 	<=	"0000000000000000";
+  sprite_snake_head_b(14) 	  	 	<=	"0000000000000000";
+  sprite_snake_head_b(15) 	  	 	<=	"0000000000000000";
+  
+    -- sprite snake body coloring
+  sprite_snake_body(0) 			<=	"0000111111110000";
+  sprite_snake_body(1) 			<=	"0001111111111000";
+  sprite_snake_body(2) 			<=	"0011111111111100";
+  sprite_snake_body(3) 			<=	"0011111111111100";
+  sprite_snake_body(4) 			<=	"0111111111111110";
+  sprite_snake_body(5) 			<=	"1111111111111111";
+  sprite_snake_body(6) 			<=	"1111111111111111";
+  sprite_snake_body(7) 			<=	"1111111111111111";
+  sprite_snake_body(8) 			<=	"1111111111111111";
+  sprite_snake_body(9) 			<=	"1111111111111111";
+  sprite_snake_body(10) 		<=	"1111111111111111";
+  sprite_snake_body(11) 		<=	"0111111111111110";
+  sprite_snake_body(12) 	   <=	"0011111111111100";
+  sprite_snake_body(13) 	   <=	"0011111111111100";
+  sprite_snake_body(14) 	   <=	"0001111111111000";
+  sprite_snake_body(15) 	   <=	"0000111111110000";
+  
+  -- sprite snake tail coloring
+  sprite_snake_tail(0) 			<=	"0000000000110000";
+  sprite_snake_tail(1) 			<=	"0000000011111000";
+  sprite_snake_tail(2) 			<=	"0000001111111100";
+  sprite_snake_tail(3) 			<=	"0000111111111100";
+  sprite_snake_tail(4) 			<=	"0001111111111110";
+  sprite_snake_tail(5) 			<=	"0011111111111110";
+  sprite_snake_tail(6) 			<=	"0111111111111111";
+  sprite_snake_tail(7) 			<=	"1111111111111111";
+  sprite_snake_tail(8) 			<=	"1111111111111111";
+  sprite_snake_tail(9) 			<=	"0111111111111111";
+  sprite_snake_tail(10) 		<=	"0011111111111110";
+  sprite_snake_tail(11) 		<=	"0001111111111110";
+  sprite_snake_tail(12) 	   <=	"0000111111111100";
+  sprite_snake_tail(13) 	   <=	"0000001111111100";
+  sprite_snake_tail(14) 	   <=	"0000000011111000";
+  sprite_snake_tail(15) 	   <=	"0000000000110000";
+  
+  -- sprite rabbit gray body coloring
+  sprite_food_rabbit_y(0) 		<=	"0000100000010000";
+  sprite_food_rabbit_y(1) 		<=	"0000110000110000";
+  sprite_food_rabbit_y(2) 		<=	"0000111001110000";
+  sprite_food_rabbit_y(3) 		<=	"0000101001010000";
+  sprite_food_rabbit_y(4) 		<=	"0000101001010000";
+  sprite_food_rabbit_y(5) 		<=	"0000101001010000";
+  sprite_food_rabbit_y(6) 		<=	"0000111111110000";
+  sprite_food_rabbit_y(7) 		<=	"0001100110011000";
+  sprite_food_rabbit_y(8) 		<=	"0001100110011000";
+  sprite_food_rabbit_y(9) 		<=	"0001100110011000";
+  sprite_food_rabbit_y(10) 	<=	"0001111111111000";
+  sprite_food_rabbit_y(11) 	<=	"0001111111111000";
+  sprite_food_rabbit_y(12) 	<=	"0001110000111000";
+  sprite_food_rabbit_y(13) 	<=	"0001111001111000";
+  sprite_food_rabbit_y(14) 	<=	"0000111111110000";
+  sprite_food_rabbit_y(15) 	<=	"0000011111100000";
+  
+    -- sprite rabbit blue eyeball coloring
+  sprite_food_rabbit_b(0) 			<=	"0000000000000000";
+  sprite_food_rabbit_b(1) 			<=	"0000000000000000";
+  sprite_food_rabbit_b(2) 			<=	"0000000000000000";
+  sprite_food_rabbit_b(3) 			<=	"0000000000000000";
+  sprite_food_rabbit_b(4) 			<=	"0000000000000000";
+  sprite_food_rabbit_b(5) 			<=	"0000000000000000";
+  sprite_food_rabbit_b(6) 			<=	"0000000000000000";
+  sprite_food_rabbit_b(7) 			<=	"0000000000000000";
+  sprite_food_rabbit_b(8) 			<=	"0000001000100000";
+  sprite_food_rabbit_b(9) 			<=	"0000001000100000";
+  sprite_food_rabbit_b(10) 		<=	"0000000000000000";
+  sprite_food_rabbit_b(11) 		<=	"0000000000000000";
+  sprite_food_rabbit_b(12) 	   <=	"0000000000000000";
+  sprite_food_rabbit_b(13) 	   <=	"0000000000000000";
+  sprite_food_rabbit_b(14) 	   <=	"0000000000000000";
+  sprite_food_rabbit_b(15) 	   <=	"0000000000000000";
+  
+  -- sprite rabbit pink nose coloring
+  sprite_food_rabbit_p(0) 			<=	"0000000000000000";
+  sprite_food_rabbit_p(1) 			<=	"0000000000000000";
+  sprite_food_rabbit_p(2) 			<=	"0000010000100000";
+  sprite_food_rabbit_p(3) 			<=	"0000010000100000";
+  sprite_food_rabbit_p(4) 			<=	"0000010000100000";
+  sprite_food_rabbit_p(5) 			<=	"0000010000100000";
+  sprite_food_rabbit_p(6) 			<=	"0000010000100000";
+  sprite_food_rabbit_p(7) 			<=	"0000010000100000";
+  sprite_food_rabbit_p(8) 			<=	"0000010000100000";
+  sprite_food_rabbit_p(9) 			<=	"0000010000100000";
+  sprite_food_rabbit_p(10) 		<=	"0000010000100000";
+  sprite_food_rabbit_p(11) 		<=	"0000010000100000";
+  sprite_food_rabbit_p(12) 	   <=	"0000001111000000";
+  sprite_food_rabbit_p(13) 	   <=	"0000000110000000";
+  sprite_food_rabbit_p(14) 	   <=	"0000000000000000";
+  sprite_food_rabbit_p(15) 	   <=	"0000000000000000";
+  
+   -- sprite rabbit eye white coloring
+  sprite_food_rabbit_w(0) 			<=	"0000000000000000";
+  sprite_food_rabbit_w(1) 			<=	"0000000000000000";
+  sprite_food_rabbit_w(2) 			<=	"0000000000000000";
+  sprite_food_rabbit_w(3) 			<=	"0000000000000000";
+  sprite_food_rabbit_w(4) 			<=	"0000000000000000";
+  sprite_food_rabbit_w(5) 			<=	"0000000000000000";
+  sprite_food_rabbit_w(6) 			<=	"0000000000000000";
+  sprite_food_rabbit_w(7) 			<=	"0000011001100000";
+  sprite_food_rabbit_w(8) 			<=	"0000010001000000";
+  sprite_food_rabbit_w(9) 			<=	"0000010001000000";
+  sprite_food_rabbit_w(10) 		<=	"0000000000000000";
+  sprite_food_rabbit_w(11) 		<=	"0000000000000000";
+  sprite_food_rabbit_w(12) 	   <=	"0000000000000000";
+  sprite_food_rabbit_w(13) 	   <=	"0000000000000000";
+  sprite_food_rabbit_w(14) 	   <=	"0000000000000000";
+  sprite_food_rabbit_w(15) 	   <=	"0000000000000000";
+  
+
   
   circle_center_h <= 350;
   circle_center_v <= 240;

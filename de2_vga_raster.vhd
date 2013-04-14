430842aa308561044e473af5e65176c84ca6ab2c
mdk2133 <mdk2133@micro15.ilab.columbia.edu>
2013-04-14 09:24:07 -0400

    remaining sprites

--- de2_vga_raster.vhd
+++ de2_vga_raster.vhd
@@ -117,16 +117,25 @@ architecture rtl of de2_vga_raster is
 	signal sprite_food_mouse_l	: array_type_16x16;
 	
 	-- edwards colorings
-	signal sprite_food_edwards	: array_type_16x16;
+	signal sprite_food_edwards_n	: array_type_16x16;
+	signal sprite_food_edwards_t	: array_type_16x16;
+	signal sprite_food_edwards_l	: array_type_16x16;
+	signal sprite_food_edwards_p	: array_type_16x16;
 	
-	signal sprite_powup_growth	: array_type_16x16;
+	-- needle growth coloring
+	signal sprite_powup_growth_r	: array_type_16x16;
+	signal sprite_powup_growth_y	: array_type_16x16;
 	
+	-- lightning speed coloring
 	signal sprite_powup_speed	: array_type_16x16;
 	
+	-- ice freeze coloring
 	signal sprite_powup_freeze	: array_type_16x16;
 	
+	-- wall obstacle coloring
 	signal sprite_wall			: array_type_16x16;
 	
+	-- letter colorings
 	signal sprite_P				: array_type_16x16;
 	signal sprite_1				: array_type_16x16;
 	signal sprite_2				: array_type_16x16;
@@ -135,6 +144,8 @@ architecture rtl of de2_vga_raster is
 	signal sprite_N				: array_type_16x16;
 	signal sprite_S				: array_type_16x16;
 	signal sprite_exclam			: array_type_16x16;
+	
+	-- pause and play colorings
 	signal sprite_pause			: array_type_16x16;
 	signal sprite_play			: array_type_16x16;
 	
@@ -322,7 +333,405 @@ begin
   sprite_food_rabbit_w(14) 	   <=	"0000000000000000";
   sprite_food_rabbit_w(15) 	   <=	"0000000000000000";
   
+  -- sprite mouse gray body coloring
+	sprite_food_mouse_y(0) 			<=	"0000100000010000";
+	sprite_food_mouse_y(1) 			<=	"0001110000111000";
+	sprite_food_mouse_y(2) 			<=	"0011111001111100";
+	sprite_food_mouse_y(3) 			<=	"0110001001000110";
+	sprite_food_mouse_y(4) 			<=	"0110001001000110";
+	sprite_food_mouse_y(5) 			<=	"0110001001000110";
+	sprite_food_mouse_y(6) 			<=	"0011111111111100";
+	sprite_food_mouse_y(7) 			<=	"0011100110011000";
+	sprite_food_mouse_y(8) 			<=	"0001100110011000";
+	sprite_food_mouse_y(9) 			<=	"0001100110011000";
+	sprite_food_mouse_y(10) 		<=	"0001111111111000";
+	sprite_food_mouse_y(11) 		<=	"0001111111111000";
+	sprite_food_mouse_y(12) 	  	<=	"0001000110001000";
+	sprite_food_mouse_y(13) 	   <=	"0001111001111000";
+	sprite_food_mouse_y(14) 	  	<=	"0000100110010000";
+	sprite_food_mouse_y(15)			<=	"0000011111100000";
 
+	-- sprite mouse pink coloring
+	sprite_food_mouse_p(0) 			<=	"0000000000000000";
+	sprite_food_mouse_p(1) 			<=	"0000000000000000";
+	sprite_food_mouse_p(2) 			<=	"0000000000000000";
+	sprite_food_mouse_p(3) 			<=	"0001110000111000";
+	sprite_food_mouse_p(4) 			<=	"0001110000111000";
+	sprite_food_mouse_p(5) 			<=	"0001110000111000";
+	sprite_food_mouse_p(6) 			<=	"0000000000000000";
+	sprite_food_mouse_p(7) 			<=	"0000000000000000";
+	sprite_food_mouse_p(8) 			<=	"0000000000000000";
+	sprite_food_mouse_p(9) 			<=	"0000000000000000";
+	sprite_food_mouse_p(10) 		<=	"0000000000000000";
+	sprite_food_mouse_p(11) 		<=	"0000000000000000";
+	sprite_food_mouse_p(12) 	  	<=	"0000000000000000";
+	sprite_food_mouse_p(13) 	   <=	"0000000000000000";
+	sprite_food_mouse_p(14) 	  	<=	"0000000000000000";
+	sprite_food_mouse_p(15)			<=	"0000000000000000";
+
+	-- sprite mouse whisker black coloring
+	sprite_food_mouse_l(0) 			<=	"0000000000000000";
+	sprite_food_mouse_l(1) 			<=	"0000000000000000";
+	sprite_food_mouse_l(2) 			<=	"0000000000000000";
+	sprite_food_mouse_l(3) 			<=	"0000000000000000";
+	sprite_food_mouse_l(4) 			<=	"0000000000000000";
+	sprite_food_mouse_l(5) 			<=	"0000000000000000";
+	sprite_food_mouse_l(6) 			<=	"0000000000000000";
+	sprite_food_mouse_l(7) 			<=	"0000000000000000";
+	sprite_food_mouse_l(8) 			<=	"0000000000000000";
+	sprite_food_mouse_l(9) 			<=	"0000000000000000";
+	sprite_food_mouse_l(10) 		<=	"0000000000000000";
+	sprite_food_mouse_l(11) 		<=	"0000000000000000";
+	sprite_food_mouse_l(12) 	  	<=	"0000111001110000";
+	sprite_food_mouse_l(13) 	   <=	"0000000110000000";
+	sprite_food_mouse_l(14) 	  	<=	"0000011001100000";
+	sprite_food_mouse_l(15)			<=	"0000000000000000";
+  
+	-- sprite food edwards hair coloring
+	sprite_food_edwards_n(0) 			<=	"0000000000000000";
+	sprite_food_edwards_n(1) 			<=	"0000011111100000";
+	sprite_food_edwards_n(2) 			<=	"0000111111110000";
+	sprite_food_edwards_n(3) 			<=	"0001111111111000";
+	sprite_food_edwards_n(4) 			<=	"0001111111111000";
+	sprite_food_edwards_n(5) 			<=	"0001000000001000";
+	sprite_food_edwards_n(6) 			<=	"0000000000000000";
+	sprite_food_edwards_n(7) 			<=	"0000000000000000";
+	sprite_food_edwards_n(8) 			<=	"0000000000000000";
+	sprite_food_edwards_n(9) 			<=	"0000000000000000";
+	sprite_food_edwards_n(10) 			<=	"0000000000000000";
+	sprite_food_edwards_n(11) 			<=	"0000000000000000";
+	sprite_food_edwards_n(12) 	  		<=	"0000000000000000";
+	sprite_food_edwards_n(13) 	   		<=	"0000000000000000";
+	sprite_food_edwards_n(14) 	  		<=	"0000000000000000";
+	sprite_food_edwards_n(15)			<=	"0000000000000000";
+
+	-- sprite food edwards skin coloring
+	sprite_food_edwards_t(0) 			<=	"0000000000000000";
+	sprite_food_edwards_t(1) 			<=	"0000000000000000";
+	sprite_food_edwards_t(2) 			<=	"0000000000000000";
+	sprite_food_edwards_t(3) 			<=	"0000000000000000";
+	sprite_food_edwards_t(4) 			<=	"0000000000000000";
+	sprite_food_edwards_t(5) 			<=	"0000111111110000";
+	sprite_food_edwards_t(6) 			<=	"0001100110011000";
+	sprite_food_edwards_t(7) 			<=	"0010000000000100";
+	sprite_food_edwards_t(8) 			<=	"0011000000001100";
+	sprite_food_edwards_t(9) 			<=	"0011000000001100";
+	sprite_food_edwards_t(10) 			<=	"0001100110011000";
+	sprite_food_edwards_t(11) 			<=	"0001111111111000";
+	sprite_food_edwards_t(12) 	  		<=	"0001111001111000";
+	sprite_food_edwards_t(13) 	   		<=	"0001111001111000";
+	sprite_food_edwards_t(14) 	  		<=	"0000111111110000";
+	sprite_food_edwards_t(15)			<=	"0000011111100000";
+
+	-- sprite food edwards lips coloring
+	sprite_food_edwards_p(0) 			<=	"0000000000000000";
+	sprite_food_edwards_p(1) 			<=	"0000000000000000";
+	sprite_food_edwards_p(2) 			<=	"0000000000000000";
+	sprite_food_edwards_p(3) 			<=	"0000000000000000";
+	sprite_food_edwards_p(4) 			<=	"0000000000000000";
+	sprite_food_edwards_p(5) 			<=	"0000000000000000";
+	sprite_food_edwards_p(6) 			<=	"0000000000000000";
+	sprite_food_edwards_p(7) 			<=	"0000000000000000";
+	sprite_food_edwards_p(8) 			<=	"0000000000000000";
+	sprite_food_edwards_p(9) 			<=	"0000000000000000";
+	sprite_food_edwards_p(10) 			<=	"0000000000000000";
+	sprite_food_edwards_p(11) 			<=	"0000000000000000";
+	sprite_food_edwards_p(12) 	  		<=	"0000000110000000";
+	sprite_food_edwards_p(13) 	   	<=	"0000000110000000";
+	sprite_food_edwards_p(14) 	  		<=	"0000000000000000";
+	sprite_food_edwards_p(15)			<=	"0000000000000000";
+
+
+	-- sprite food edwards glasses black coloring
+	sprite_food_edwards_l(0) 			<=	"0000000000000000";
+	sprite_food_edwards_l(1) 			<=	"0000000000000000";
+	sprite_food_edwards_l(2) 			<=	"0000000000000000";
+	sprite_food_edwards_l(3) 			<=	"0000000000000000";
+	sprite_food_edwards_l(4) 			<=	"0000000000000000";
+	sprite_food_edwards_l(5) 			<=	"0000000000000000";
+	sprite_food_edwards_l(6) 			<=	"0010011001100100";
+	sprite_food_edwards_l(7) 			<=	"0001100110011000";
+	sprite_food_edwardss_l(8) 			<=	"0000100110010000";
+	sprite_food_edwards_l(9) 			<=	"0000100110010000";
+	sprite_food_edwards_l(10) 			<=	"0000011001100000";
+	sprite_food_edwards_l(11) 			<=	"0000000000000000";
+	sprite_food_edwards_l(12) 	  		<=	"0000000000000000";
+	sprite_food_edwards_l(13) 	   	<=	"0000000000000000";
+	sprite_food_edwards_l(14) 	  		<=	"0000000000000000";
+	sprite_food_edwards_l(15)			<=	"0000000000000000";
+
+  
+	-- sprite needle growth blood coloring
+	sprite_powup_growth_r(0) 			<=	"0000000000000000";
+	sprite_powup_growth_r(1) 			<=	"0000000000000000";
+	sprite_powup_growth_r(2) 			<=	"0000000000000000";
+	sprite_powup_growth_r(3) 			<=	"0000000000000000";
+	sprite_powup_growth_r(4) 			<=	"0000000000000000";
+	sprite_powup_growth_r(5) 			<=	"0000011000000000";
+	sprite_powup_growth_r(6) 			<=	"0000011100000000";
+	sprite_powup_growth_r(7) 			<=	"0000001110000000";
+	sprite_powup_growth_r(8) 			<=	"0000000111000000";
+	sprite_powup_growth_r(9) 			<=	"0000000011100000";
+	sprite_powup_growth_r(10) 			<=	"0000000001100000";
+	sprite_powup_growth_r(11) 			<=	"0000000000000000";
+	sprite_powup_growth_r(12) 	  		<=	"0000000000000000";
+	sprite_powup_growth_r(13) 	   	<=	"0000000000000000";
+	sprite_powup_growth_r(14) 	  		<=	"0000000000000000";
+	sprite_powup_growth_r(15)			<=	"0000000000000000";
+
+	-- sprite needle growth gray coloring
+	sprite_powup_growth_y(0) 			<=	"1000000000000000";
+	sprite_powup_growth_y(1) 			<=	"0100000000000000";
+	sprite_powup_growth_y(2) 			<=	"0010000000000000";
+	sprite_powup_growth_y(3) 			<=	"0001000000000000";
+	sprite_powup_growth_y(4) 			<=	"0000111000000000";
+	sprite_powup_growth_y(5) 			<=	"0000100100000000";
+	sprite_powup_growth_y(6) 			<=	"0000100010000000";
+	sprite_powup_growth_y(7) 			<=	"0000010001000000";
+	sprite_powup_growth_y(8) 			<=	"0000001000100000";
+	sprite_powup_growth_y(9) 			<=	"0000000100010100";
+	sprite_powup_growth_y(10) 			<=	"0000000010011000";
+	sprite_powup_growth_y(11) 			<=	"0000000001110000";
+	sprite_powup_growth_y(12) 	  		<=	"0000000000101000";
+	sprite_powup_growth_y(13) 	   	<=	"0000000001000111";
+	sprite_powup_growth_y(14) 	  		<=	"0000000000000110";
+	sprite_powup_growth_y(15)			<=	"0000000000000100";
+
+	-- sprite lightning coloring
+	sprite_powup_speed(0) 			<=	"0000000011111111";
+	sprite_powup_speed(1) 			<=	"0000000111111110";
+	sprite_powup_speed(2) 			<=	"0000001111111100";
+	sprite_powup_speed(3) 			<=	"0000011111111000";
+	sprite_powup_speed(4) 			<=	"0000111111110000";
+	sprite_powup_speed(5) 			<=	"0001111111100000";
+	sprite_powup_speed(6) 			<=	"0011111111111110";
+	sprite_powup_speed(7) 			<=	"0011111111111100";
+	sprite_powup_speed(8) 			<=	"0011111111111000";
+	sprite_powup_speed(9) 			<=	"0000011111110000";
+	sprite_powup_speed(10) 			<=	"0000011111100000";
+	sprite_powup_speed(11) 			<=	"0000111111000000";
+	sprite_powup_speed(12) 	  		<=	"0000111111000000";
+	sprite_powup_speed(13) 	   	<=	"0000111110000000";
+	sprite_powup_speed(14) 	  		<=	"0001111000000000";
+	sprite_powup_speed(15)			<=	"0001110000000000";
+
+	-- sprite ice freeze coloring
+	sprite_powup_freeze(0) 			<=	"1100001000100011";
+	sprite_powup_freeze(1) 			<=	"0110011000110110";
+	sprite_powup_freeze(2) 			<=	"0011110000011100";
+	sprite_powup_freeze(3) 			<=	"0001100000011110";
+	sprite_powup_freeze(4) 			<=	"0011110000110011";
+	sprite_powup_freeze(5) 			<=	"0110011001100000";
+	sprite_powup_freeze(6) 			<=	"1100001111000000";
+	sprite_powup_freeze(7) 			<=	"0000000110000000";
+	sprite_powup_freeze(8) 			<=	"0000000110000000";
+	sprite_powup_freeze(9) 			<=	"0000001111000000";
+	sprite_powup_freeze(10) 		<=	"0000011001100000";
+	sprite_powup_freeze(11) 		<=	"1100110000110011";
+	sprite_powup_freeze(12) 	  	<=	"0111100000011110";
+	sprite_powup_freeze(13) 	   <=	"0011100000011100";
+	sprite_powup_freeze(14) 	  	<=	"0110110000110110";
+	sprite_powup_freeze(15)			<=	"1100010000100011";
+
+	
+		-- sprite brick wall coloring
+	sprite_wall(0) 			<=	"1111101111101111";
+	sprite_wall(1) 			<=	"1111101111101111";
+	sprite_wall(2) 			<=	"1111101111101111";
+	sprite_wall(3) 			<=	"1111101111101111";
+	sprite_wall(4) 			<=	"0000000000000000";
+	sprite_wall(5) 			<=	"1011111011111011";
+	sprite_wall(6) 			<=	"1011111011111011";
+	sprite_wall(7) 			<=	"1011111011111011";
+	sprite_wall(8) 			<=	"0000000000000000";
+	sprite_wall(9) 			<=	"1111011111011111";
+	sprite_wall(10) 			<=	"1111011111011111";
+	sprite_wall(11) 			<=	"1111011111011111";
+	sprite_wall(12) 	  		<=	"0000000000000000";
+	sprite_wall(13) 	   	<=	"1111101111101111";
+	sprite_wall(14) 	  		<=	"1111101111101111";
+	sprite_wall(15)			<=	"1111101111101111";
+
+	-- sprite letter p
+	sprite_P(0) 			<=	"0000000000000000";
+	sprite_P(1) 			<=	"0000011111100000";
+	sprite_P(2) 			<=	"0000111111110000";
+	sprite_P(3) 			<=	"0000110001110000";
+	sprite_P(4) 			<=	"0000110000110000";
+	sprite_P(5) 			<=	"0000110000110000";
+	sprite_P(6) 			<=	"0000110001110000";
+	sprite_P(7) 			<=	"0000110011110000";
+	sprite_P(8) 			<=	"0000111111100000";
+	sprite_P(9) 			<=	"0000111111000000";
+	sprite_P(10) 			<=	"0000110000000000";
+	sprite_P(11) 			<=	"0000110000000000";
+	sprite_P(12) 	  		<=	"0000110000000000";
+	sprite_P(13) 	   	<=	"0000110000000000";
+	sprite_P(14) 	  		<=	"0000110000000000";
+	sprite_P(15)			<=	"0000000000000000";
+
+	-- sprite number 1
+	sprite_1(0) 			<=	"0000000000000000";
+	sprite_1(1) 			<=	"0000001111000000";
+	sprite_1(2) 			<=	"0000011111000000";
+	sprite_1(3) 			<=	"0000111111000000";
+	sprite_1(4) 			<=	"0000110111000000";
+	sprite_1(5) 			<=	"0000000111000000";
+	sprite_1(6) 			<=	"0000000111000000";
+	sprite_1(7) 			<=	"0000000111000000";
+	sprite_1(8) 			<=	"0000000111000000";
+	sprite_1(9) 			<=	"0000000111000000";
+	sprite_1(10) 			<=	"0000000111000000";
+	sprite_1(11) 			<=	"0000000111000000";
+	sprite_1(12) 	  		<=	"0000011111110000";
+	sprite_1(13) 	   	<=	"0000011111110000";
+	sprite_1(14) 	  		<=	"0000011111110000";
+	sprite_1(15)			<=	"0000000000000000";
+
+	-- sprite number 2
+	sprite_2(0) 			<=	"0000000000000000";
+	sprite_2(1) 			<=	"0000001111100000";
+	sprite_2(2) 			<=	"0000011111110000";
+	sprite_2(3) 			<=	"0000111000110000";
+	sprite_2(4) 			<=	"0000110000110000";
+	sprite_2(5) 			<=	"0000000001110000";
+	sprite_2(6) 			<=	"0000000001110000";
+	sprite_2(7) 			<=	"0000000011100000";
+	sprite_2(8) 			<=	"0000000111000000";
+	sprite_2(9) 			<=	"0000001110000000";
+	sprite_2(10) 			<=	"0000001110000000";
+	sprite_2(11) 			<=	"0000011100000000";
+	sprite_2(12) 	  		<=	"0000111000000000";
+	sprite_2(13) 	   	<=	"0000111111110000";
+	sprite_2(14) 	  		<=	"0000111111110000";
+	sprite_2(15)			<=	"0000000000000000";
+
+	-- sprite letter W
+	sprite_W(0) 			<=	"0000000000000000";
+	sprite_W(1) 			<=	"0010000000001000";
+	sprite_W(2) 			<=	"0110000000001100";
+	sprite_W(3) 			<=	"0110000100001100";
+	sprite_W(4) 			<=	"0110001110001100";
+	sprite_W(5) 			<=	"0110001110001100";
+	sprite_W(6) 			<=	"0110001110001100";
+	sprite_W(7) 			<=	"0110011111001100";
+	sprite_W(8) 			<=	"0110011011001100";
+	sprite_W(9) 			<=	"0110011011001100";
+	sprite_W(10) 			<=	"0111011011011100";
+	sprite_W(11) 			<=	"0111111011111100";
+	sprite_W(12) 	  		<=	"0111111011111100";
+	sprite_W(13) 	   	<=	"0011110001111000";
+	sprite_W(14) 	  		<=	"0001100000110000";
+	sprite_W(15)			<=	"0000000000000000";
+
+	-- sprite letter I
+	sprite_I(0) 			<=	"0000000000000000";
+	sprite_I(1) 			<=	"0000111111110000";
+	sprite_I(2) 			<=	"0000111111110000";
+	sprite_I(3) 			<=	"0000000110000000";
+	sprite_I(4) 			<=	"0000000110000000";
+	sprite_I(5) 			<=	"0000000110000000";
+	sprite_I(6) 			<=	"0000000110000000";
+	sprite_I(7) 			<=	"0000000110000000";
+	sprite_I(8) 			<=	"0000000110000000";
+	sprite_I(9) 			<=	"0000000110000000";
+	sprite_I(10) 			<=	"0000000110000000";
+	sprite_I(11) 			<=	"0000000110000000";
+	sprite_I(12) 	  		<=	"0000000110000000";
+	sprite_I(13) 	   	<=	"0000111111110000";
+	sprite_I(14) 	  		<=	"0000111111110000";
+	sprite_I(15)			<=	"0000000000000000";
+
+	-- sprite letter N
+	sprite_N(0) 			<=	"0000000000000000";
+	sprite_N(1) 			<=	"0001100000011000";
+	sprite_N(2) 			<=	"0001110000011000";
+	sprite_N(3) 			<=	"0001110000011000";
+	sprite_N(4) 			<=	"0001111000011000";
+	sprite_N(5) 			<=	"0001111100011000";
+	sprite_N(6) 			<=	"0001101110011000";
+	sprite_N(7) 			<=	"0001101110011000";
+	sprite_N(8) 			<=	"0001100111011000";
+	sprite_N(9) 			<=	"0001100011111000";
+	sprite_N(10) 			<=	"0001100011111000";
+	sprite_N(11) 			<=	"0001100001111000";
+	sprite_N(12) 	  		<=	"0001100000111000";
+	sprite_N(13) 	   	<=	"0001100000011000";
+	sprite_N(14) 	  		<=	"0000000000000000";
+	sprite_N(15)			<=	"0000000000000000";
+
+	-- sprite letter S
+	sprite_S(0) 			<=	"0000011111000000";
+	sprite_S(1) 			<=	"0000111111110000";
+	sprite_S(2) 			<=	"0001110011111000";
+	sprite_S(3) 			<=	"0011100000111000";
+	sprite_S(4) 			<=	"0011100000000000";
+	sprite_S(5) 			<=	"0001110000000000";
+	sprite_S(6) 			<=	"0000111000000000";
+	sprite_S(7) 			<=	"0000111110000000";
+	sprite_S(8) 			<=	"0000001111100000";
+	sprite_S(9) 			<=	"0000000001110000";
+	sprite_S(10) 			<=	"0000000000111000";
+	sprite_S(11) 			<=	"0011100000111000";
+	sprite_S(12) 	  		<=	"0011100001110000";
+	sprite_S(13) 	   	<=	"0001110011100000";
+	sprite_S(14) 	  		<=	"0000111111000000";
+	sprite_S(15)			<=	"0000011110000000";
+
+	-- sprite exclamation point
+	sprite_exclam(0) 			<=	"0000000110000000";
+	sprite_exclam(1) 			<=	"0000001111000000";
+	sprite_exclam(2) 			<=	"0000001111000000";
+	sprite_exclam(3) 			<=	"0000001111000000";
+	sprite_exclam(4) 			<=	"0000001111000000";
+	sprite_exclam(5) 			<=	"0000001111000000";
+	sprite_exclam(6) 			<=	"0000001111000000";
+	sprite_exclam(7) 			<=	"0000001111000000";
+	sprite_exclam(8) 			<=	"0000001111000000";
+	sprite_exclam(9) 			<=	"0000000110000000";
+	sprite_exclam(10) 		<=	"0000000110000000";
+	sprite_exclam(11) 		<=	"0000000000000000";
+	sprite_exclam(12) 	  	<=	"0000000110000000";
+	sprite_exclam(13) 	   <=	"0000001111000000";
+	sprite_exclam(14) 	  	<=	"0000001111000000";
+	sprite_exclam(15)			<=	"0000000110000000";
+
+	-- sprite pause button
+	sprite_pause(0) 			<=	"0000000000000000";
+	sprite_pause(1) 			<=	"0001110000111000";
+	sprite_pause(2) 			<=	"0001110000111000";
+	sprite_pause(3) 			<=	"0001110000111000";
+	sprite_pause(4) 			<=	"0001110000111000";
+	sprite_pause(5) 			<=	"0001110000111000";
+	sprite_pause(6) 			<=	"0001110000111000";
+	sprite_pause(7) 			<=	"0001110000111000";
+	sprite_pause(8) 			<=	"0001110000111000";
+	sprite_pause(9) 			<=	"0001110000111000";
+	sprite_pause(10) 			<=	"0001110000111000";
+	sprite_pause(11) 			<=	"0001110000111000";
+	sprite_pause(12) 	  		<=	"0001110000111000";
+	sprite_pause(13) 	   	<=	"0001110000111000";
+	sprite_pause(14) 	  		<=	"0001110000111000";
+	sprite_pause(15)			<=	"0000000000000000";
+
+	-- sprite play button
+	sprite_play(0) 			<=	"0001100000000000";
+	sprite_play(1) 			<=	"0001110000000000";
+	sprite_play(2) 			<=	"0001111000000000";
+	sprite_play(3) 			<=	"0001111100000000";
+	sprite_play(4) 			<=	"0001111110000000";
+	sprite_play(5) 			<=	"0001111111000000";
+	sprite_play(6) 			<=	"0001111111100000";
+	sprite_play(7) 			<=	"0001111111110000";
+	sprite_play(8) 			<=	"0001111111110000";
+	sprite_play(9) 			<=	"0001111111100000";
+	sprite_play(10) 			<=	"0001111111000000";
+	sprite_play(11) 			<=	"0001111110000000";
+	sprite_play(12) 	  		<=	"0001111100000000";
+	sprite_play(13) 	   	<=	"0001111000000000";
+	sprite_play(14) 	  		<=	"0001110000000000";
+	sprite_play(15)			<=	"0001100000000000";
+	  
   
   circle_center_h <= 350;
   circle_center_v <= 240;

1c5cb42cf711eae0ba2383b2210d96c2ea34d3e0
Ilan <ie2147@micro9.ilab.columbia.edu>
2013-04-25 21:56:32 -0400

    Changes to NES FSM stuff

--- NES_FSM/fsm_top.vhd
+++ NES_FSM/fsm_top.vhd
@@ -14,16 +14,16 @@ use IEEE.numeric_std.all;
 entity fsm_top is
 
   port (
-    -- Clocks
+    -- Clocks 
 
-    CLOCK_50	: in std_logic;	                   -- 50 MHz
+    CLOCK_50	: in std_logic;	                   		-- 50 MHz
 
-    LEDR : out std_logic_vector(17 downto 0);      -- Red LEDs
+    LEDR 		: out std_logic_vector(17 downto 0);      -- Red LEDs
     
     -- General-purpose I/O
     
-    GPIO_0,                                      -- GPIO Connection 0
-    GPIO_1 : inout std_logic_vector(35 downto 0) -- GPIO Connection 1   
+    GPIO_0,                                      		-- GPIO Connection 0
+    GPIO_1 		: inout std_logic_vector(35 downto 0) 	-- GPIO Connection 1   
     ); 
   
 end fsm_top;
@@ -31,18 +31,36 @@ end fsm_top;
 architecture datapath of fsm_top is
 
 	signal counter : integer;
+	signal reset_n : std_logic;
+	signal was_reset: std_logic := '0';
+	
+	signal buttons1, buttons2 : std_logic_vector(7 downto 0);
 
 begin
 
+	process (CLOCK_50)
+		begin
+			if rising_edge(CLOCK_50) then
+			if was_reset = '0' then
+				reset_n <= '0';
+				was_reset <= '1';
+			else
+				reset_n <= '1';
+			end if;
+		end if;
+	end process;
 
 	controller : entity work.controller port map(
 		clk			=> CLOCK_50,
+		reset 		=> not reset_n,
 		latch1	 	=> GPIO_0(2), 
 		pulse1		=> GPIO_0(4),	
 		data1			=> GPIO_0(6),	
 		latch2	 	=> GPIO_0(28), 
 		pulse2		=> GPIO_0(30),	
 		data2			=> GPIO_0(32),	
+		buttons1_out		=> buttons1,
+		buttons2_out		=> buttons2
 	  );
 	
 

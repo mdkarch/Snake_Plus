ba04275e3ee3eb4d51322254886ebeb1d220c771
jc3790 <jc3790@micro16.ilab.columbia.edu>
2013-05-09 12:31:24 -0400

    updated constants.h

--- software/snake_software/constants.h
+++ software/snake_software/constants.h
@@ -1,7 +1,6 @@
 #ifndef _CONSTANTS_H_
 #define _CONSTANTS_H_
 
-
 #define LENGTH		1200
 #define col_offset  14
 #define left_dir	0
@@ -9,22 +8,24 @@
 #define up_dir		2
 #define down_dir	3
 #define LEFT_BOUND	0
-#define RIGHT_BOUND	640
-#define BOT_BOUND	480
+#define RIGHT_BOUND	40//640
+#define BOT_BOUND	30//480
 #define TOP_BOUND	0
 
 #define X_LEN		40
 #define Y_LEN		30
-#define MAX_POWERUP_SIZE	600
+#define MAX_POWERUP_SIZE	300
+
 
 unsigned int n_seed = 5323;
+unsigned int seed = 0;
 
 int SNAKE_SIZE = 1200;
 
 
-int SLEEP_TIME 				= 10; 							// milliseconds
+int SLEEP_TIME 				= 10; 	// milliseconds
 int DEFAULT_SLEEP_CYCLE		= 60;
-int SPEED_SLEEP_CYCLE		= 40;
+int SPEED_SLEEP_CYCLE		= 30;
 int FREEZE_SLEEP_CYCLE		= 150;
 int SPEED_TIME 				= 50;
 int FREEZE_TIME 			= 50;
@@ -33,65 +34,79 @@ int PLAYER1_SLEEP_CYCLES; 			// Original sleep time/SLEEP_TIME
 int PLAYER2_SLEEP_CYCLES; 			// Original sleep time/SLEEP_TIME
 
 /* index to know which sprite to draw from power up structs */
-int food_index = 1;
-int speed_index = 1;
-int freeze_index = 1;
-int edwards_index = 1;
-int alt_powups = 0;
-/* keeps track of current power ups spwan locations */
+int food_index 				= 1;
+int speed_index 			= 1;
+int freeze_index 			= 1;
+int edwards_index 			= 1;
+int alt_powups 				= 0;
+
+/* keeps track of current power ups spawn locations */
 int board[X_LEN][Y_LEN];
 /* keeps track of where a brick was placed */
 short brick_tiles[X_LEN][Y_LEN];
-int freeze_pow_count  = 0;
-int freeze_drawn = 0;
-int speed_pow_count  = 0;
-int speed_drawn = 0;
 
+int freeze_pow_count  		= 0;
+int freeze_drawn 			= 0;
+int speed_pow_count  		= 0;
+int speed_drawn 			= 0;
+int dir_arg 				= 0;
+int switch_snakes			= 0;
+int food_count = 0;
+
+
+/*
+ * 0 -> nobody yet
+ * 1 -> snake1
+ * 2 -> snake2
+ * 3 -> draw
+ */
+int game_winner				= 0;
+
+/* struct defs for snake and power ups */
 struct Snake{
-	int xCoord;
-	int yCoord;
-	int enable;
+	short xCoord;
+	short yCoord;
+	short enable;
 };
 
 struct SnakeInfo{
-	int head;
-	int tail;
-	int speed_count;
-	int speed_enabled;
-	int freeze_count;
-	int freeze_enabled;
-	int has_edwards;
-	int has_freeze;
+	short head;
+	short tail;
+	short speed_count;
+	short speed_enabled;
+	short freeze_count;
+	short freeze_enabled;
+	short has_edwards;
+	short has_freeze;
 };
 
 struct Food{
-	int xCoord;
-	int yCoord;
-	int enable;
-	int type;
+	short xCoord;
+	short yCoord;
+	short enable;
+	short type;
 };
 
-
 struct Speed{
-	int xCoord;
-	int yCoord;
-	int enable;
+	short xCoord;
+	short yCoord;
+	short enable;
 };
 
 struct Freeze{
-	int xCoord;
-	int yCoord;
-	int enable;
+	short xCoord;
+	short yCoord;
+	short enable;
 };
 
 struct Edwards{
-	int xCoord;
-	int yCoord;
-	int enable;
-	int type;
+	short xCoord;
+	short yCoord;
+	short enable;
+	short type;
 };
 
-
+/* gloabl structs */
 struct Food food[MAX_POWERUP_SIZE];
 struct Speed speed[MAX_POWERUP_SIZE];
 struct Freeze freeze[MAX_POWERUP_SIZE];
